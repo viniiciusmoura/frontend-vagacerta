2995b6e1a88c08ef094062456db8697b17eb49d9
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-12-03 12:39:26 -0300

    Criando as view das configuraçoes do menu

---
+++ src/views/Configuracao-candidato.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer
+          expand-on-hover
+          rail
+        >
+          <v-list>
+            <v-list-item
+              prepend-avatar="https://static.thenounproject.com/png/1819843-200.png"
+              title="NOME"
+              subtitle="EMAIL@gmailcom"
+            ></v-list-item>
+          </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-list density="compact" nav>
+            <v-list-item @click="showContent('Meu perfil')" prepend-icon="mdi-account-edit" title="Meu perfil" value="meuperfil"></v-list-item>
+            <v-list-item @click="showContent('Meu endereço')" prepend-icon="mdi-home-edit" title="Meu endereço" value="meuendereco"></v-list-item>
+            <v-list-item @click="showContent('Minha experiecia')" prepend-icon="mdi-briefcase" title="Minha experiecia" value="Minhaexperiecia"></v-list-item>
+            <v-list-item @click="showContent('Minhas vagas')" prepend-icon="mdi-briefcase-outline" title="Inscriçao de vagas" value="minhasvagas"></v-list-item>
+            <v-divider></v-divider>
+            <v-list-item prepend-icon="mdi-exit-to-app" title="Sair" value="sair"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+  
+        <v-main style="height: 800px">
+          <v-container v-if="selectedContent">
+            <v-layout align-center justify-center>
+              <v-text-display1>{{ selectedContent }}</v-text-display1>
+            </v-layout>
+          </v-container>
+        </v-main>
+      </v-layout>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedContent: null,
+      };
+    },
+    methods: {
+      showContent(content) {
+        this.selectedContent = content;
+      },
+    },
+  };
+  </script>
+  
---
+++ src/views/Configuracao-empresa.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://static.thenounproject.com/png/1819843-200.png"
+            title="EMPRESA"
+            subtitle="CNPJ"
+            
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item @click="showContent('Meu perfil')" prepend-icon="mdi-account-edit" title="Meu perfil" value="meuperfil"></v-list-item>
+          <v-list-item @click="showContent('Meu endereço')" prepend-icon="mdi-home-edit" title="Meu endereço" value="meuendereco"></v-list-item>
+          <v-list-item @click="showContent('Minhas vagas')" prepend-icon="mdi-briefcase" title="Minhas vagas" value="minhavaga"></v-list-item>
+          <v-divider></v-divider>
+          <v-list-item prepend-icon="mdi-exit-to-app" title="Sair" value="sair"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 800px">
+        <v-container v-if="selectedContent">
+          <v-layout align-center justify-center>
+            <v-text-display1>{{ selectedContent }}</v-text-display1>
+          </v-layout>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedContent: null,
+    };
+  },
+  methods: {
+    showContent(content) {
+      this.selectedContent = content;
+    },
+  },
+};
+</script>
