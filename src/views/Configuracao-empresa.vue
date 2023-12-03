931df5b08b9f99e0ef2b3855bc94480133787306
Erick-Macedo-Costa <erickpart28@gmail.com>
2023-12-03 17:28:54 -0300

    Arrumando as tabala e form

--- src/views/Configuracao-empresa.vue
+++ src/views/Configuracao-empresa.vue
@@ -8,45 +8,56 @@
         <v-list>
           <v-list-item
             prepend-avatar="https://static.thenounproject.com/png/1819843-200.png"
-            title="EMPRESA"
-            subtitle="CNPJ"
-            
-          ></v-list-item>
+            title="NOME"
+            subtitle="EMAIL@gmailcom"
+            two-line
+          >
+            <v-list-item-subtitle>CNPJ</v-list-item-subtitle>
+          </v-list-item>
+
+          <v-list-item @click="componetIsCurrent(tabela)" prepend-icon="mdi-briefcase" title="Minhas vagas" value="minhavaga"></v-list-item>
         </v-list>
 
         <v-divider></v-divider>
 
         <v-list density="compact" nav>
-          <v-list-item @click="showContent('Meu perfil')" prepend-icon="mdi-account-edit" title="Meu perfil" value="meuperfil"></v-list-item>
-          <v-list-item @click="showContent('Meu endereço')" prepend-icon="mdi-home-edit" title="Meu endereço" value="meuendereco"></v-list-item>
-          <v-list-item @click="showContent('Minhas vagas')" prepend-icon="mdi-briefcase" title="Minhas vagas" value="minhavaga"></v-list-item>
+          <v-list-item  @click="componetIsCurrent(stepTheeCompany)"  prepend-icon="mdi-account-edit" title="Meu perfil" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-home-edit" title="Meu endereço" value="shared"></v-list-item>
           <v-divider></v-divider>
-          <v-list-item prepend-icon="mdi-exit-to-app" title="Sair" value="sair"></v-list-item>
+          <v-list-item prepend-icon="mdi-exit-to-app" title="Sair" value="starred"></v-list-item>
         </v-list>
       </v-navigation-drawer>
 
       <v-main style="height: 800px">
-        <v-container v-if="selectedContent">
-          <v-layout align-center justify-center>
-            <v-text-display1>{{ selectedContent }}</v-text-display1>
-          </v-layout>
+        <v-container>
+          <component :is="current" :userData=userData />
         </v-container>
       </v-main>
     </v-layout>
   </v-card>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      selectedContent: null,
-    };
-  },
-  methods: {
-    showContent(content) {
-      this.selectedContent = content;
-    },
-  },
-};
+<script setup lang="ts">
+import tabela from '@/components/company/listVacancies.vue';
+import { ref } from 'vue';
+import stepTheeCompany from '@/components/register/company/stepTheeCompany.vue';
+import {UserRegister } from '@/types/register.types';
+
+const current=ref<any>();
+
+const userData = ref<UserRegister>({
+    typeAccount: null,
+    user: {
+        email: '',
+        typeRegister: '',
+        password: '',
+        termService: false,
+        termUser: true
+    }
+});
+
+function componetIsCurrent(componet:any){
+  current.value=componet
+
+}
 </script>
